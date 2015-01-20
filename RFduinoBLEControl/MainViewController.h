b546d2953c520499d13d9b6ea394bb72e34ddbbb
nineten <jayden87@gmail.com>
2015-01-20 14:26:46 +0800

    Added a main view controller

---
+++ RFduinoBLEControl/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  RFduinoBLEControl
+//
+//  Created by Jayden.Ma on 20/1/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface MainViewController : UIViewController
+
+@property AppDelegate* delegate;
+
+@end
--- RFduinoBLEControl/MainViewController.m
+++ RFduinoBLEControl/MainViewController.m
@@ -1,27 +1,29 @@
 //
-//  ViewController.m
+//  MainViewController.m
 //  RFduinoBLEControl
 //
 //  Created by Jayden.Ma on 20/1/15.
 //
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    self.delegate = (AppDelegate*)[[UIApplication sharedApplication] delegate];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
