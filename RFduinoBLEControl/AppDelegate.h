0fb30a5d33227d8239f42723c9dc9a30a8cf58fa
nineten <jayden87@gmail.com>
2015-01-20 14:07:14 +0800

    Init with core bluetooth stub methods.

--- RFduinoBLEControl/AppDelegate.h
+++ RFduinoBLEControl/AppDelegate.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, CBCentralManagerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
--- RFduinoBLEControl/AppDelegate.m
+++ RFduinoBLEControl/AppDelegate.m
@@ -124,4 +124,26 @@
     }
 }
 
+#pragma mark - Core Bluetooth
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
+
+}
+
 @end
